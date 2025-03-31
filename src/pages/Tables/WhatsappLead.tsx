cd08f45e00227bcecfdb59f0a7bc819cae2cfbef
Caous <caous.g@gmail.com>
2025-03-31 06:56:37 -0300

    references

--- src/pages/Tables/WhatsappLead.tsx
+++ src/pages/Tables/WhatsappLead.tsx
@@ -7,8 +7,8 @@ export default function BasicTables() {
   return (
     <>
       <PageMeta
-        title="React.js Basic Tables Dashboard | TailAdmin - Next.js Admin Dashboard Template"
-        description="This is React.js Basic Tables Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+        title="Instituto Barros - WhatsappLead"
+        description="Página do sistema Instituto Barros, para gerenciamento de lead"
       />
       <PageBreadcrumb pageTitle="Whatsapp" />
       <div className="space-y-6">
